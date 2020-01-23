e1177557478932dbf03d3283dbb83aa54fe41022
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-01-23 21:22:54 +0600

    add solution

---
+++ DesignPatterns/DesignPatterns/__Commons/CsvFileReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DesignPatterns.Commons
+{
+    public class CsvFileReader
+    {
+        public static IList<string[]> Read(String filepath)
+        {
+            return File.ReadAllLines(filepath).Select(s => s.Split(",")).ToList();
+        }
+    }
+}
