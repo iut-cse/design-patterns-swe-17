ff02ae595bc31e4ac6cf225715187f69ff28e088
XLR8_07 <joyashikur@gmail.com>
2020-01-24 00:05:23 +0600

    Test Added for ItemCount

--- 85-joy/LAB 01/FileParser/src/UnitTests.java
+++ 85-joy/LAB 01/FileParser/src/UnitTests.java
@@ -3,4 +3,13 @@ import org.testng.annotations.Test;
 
 public class UnitTests {
 
+    @Test
+    public void ItemCountTest()
+    {
+        Factory factory = new Factory();
+        IFileParser fileParser = factory.CreateFileParser("E:\\A C A D E M I C\\IUT SWE\\SEM 5\\SWE    4501         DESIGN PATTERNS\\L A B\\design-patterns-swe-17\\85-joy\\LAB 01\\FileParser\\dataset\\example.scsv");
+
+        assertEquals(3,fileParser.GetItemCount());
+    }
+
 }
