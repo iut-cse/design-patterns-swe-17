022dcee17d94738d9ab672012a5faffc99de041c
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-01-23 21:30:50 +0600

    add class hour model

---
+++ DesignPatterns/DesignPatterns/_00_Mohayemin/Reports/ClassHourModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DesignPatterns._00_Mohayemin.Reports
+{
+    class ClassHourModel
+    {
+        public string Department { get; set; }
+        public DateTime Date { get; set; }
+        public int ClassHour { get; set; }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/_00_Mohayemin/Reports/ReportTable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DesignPatterns._00_Mohayemin.Reports
+{
+    class ReportTable
+    {
+        private readonly IList<ClassHourModel> classHours;
+
+        public ReportTable(IList<ClassHourModel> classHours)
+        {
+            this.classHours = classHours;
+        }
+
+        public string Render()
+        {
+            return null;
+        }
+    }
+}
