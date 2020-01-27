51a9c40e398ffd6dc9c8f3607dc12ad4115c7a8b
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-01-28 05:16:14 +0600

    render report without formatting

--- DesignPatterns/DesignPatterns/_00_Mohayemin/Reports/ReportTable.cs
+++ DesignPatterns/DesignPatterns/_00_Mohayemin/Reports/ReportTable.cs
@@ -1,20 +1,83 @@
-﻿using System.Collections;
+﻿using static System.DayOfWeek;
 using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
 
 namespace DesignPatterns._00_Mohayemin.Reports
 {
-    class ReportTable
+    class ReportTable: IRenderable
     {
-        private readonly IList<ClassHourModel> classHours;
+        private readonly List<ReportRow> rows;
 
-        public ReportTable(IList<ClassHourModel> classHours)
+        public ReportTable(List<ClassHour> classHours)
         {
-            this.classHours = classHours;
+            var departments = classHours.ConvertAll(ch => ch.department).Distinct().ToList();
+            rows = departments.ConvertAll(d => new ReportRow(d, classHours.FindAll(ch => ch.department == d)));
         }
 
-        public string Render()
+        public void Render(StringBuilder builder)
         {
-            return null;
+            foreach (var row in rows)
+            {
+                row.Render(builder);
+                builder.Append("\n");
+            }
         }
+    }
+
+    class ReportRow: IRenderable
+    {
+        private readonly ReportCell departmentCell;
+        private readonly List<ReportCell> dayCells;
+        private readonly ReportCell totalCell;
+
+        public ReportRow(string department, List<ClassHour> classHours)
+        {
+            departmentCell = new ReportCell(department);
+            dayCells = new List<ReportCell>();
+            var groupedClassHours = classHours.GroupBy(ch => ch.date.DayOfWeek).ToDictionary(g => g.Key, g => g.Sum(ch => ch.durationHours));
+            foreach (var dow in AllDaysOfWeek.FromMonday)
+            {
+                var cellValue = groupedClassHours.ContainsKey(dow) ? groupedClassHours[dow] : 0;
+                dayCells.Add(new ReportCell(cellValue.ToString()));
+            }
+
+            totalCell = new ReportCell(classHours.Sum(ch => ch.durationHours).ToString());
+        }
+
+        public void Render(StringBuilder builder)
+        {
+            builder.Append("|");
+            departmentCell.Render(builder);
+            foreach (var dc in dayCells)
+                dc.Render(builder);
+
+            totalCell.Render(builder);
+        }
+    }
+
+    class ReportCell: IRenderable
+    {
+        public string Content { get; }
+        public ReportCell(string content)
+        {
+            Content = content;
+        }
+
+        public void Render(StringBuilder builder)
+        {
+            builder.Append($" {Content} |");
+        }
+    }
+
+    interface IRenderable
+    {
+        void Render(StringBuilder builder);
+    }
+
+    static class AllDaysOfWeek
+    {
+        public static DayOfWeek[] FromMonday = new[] { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
     }
 }
---
+++ DesignPatterns/DesignPatterns/_00_Mohayemin/Reports/ReportTableTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DesignPatterns._00_Mohayemin.Reports
+{
+    public class ReportTableTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public ReportTableTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+        [Fact]
+        void X()
+        {
+            var data = new List<ClassHour>
+            {
+                new ClassHour("Physics", new DateTime(2020, 1, 20), 4),
+                new ClassHour("Physics", new DateTime(2020, 1, 27), 5),
+                new ClassHour("Chemistry", new DateTime(2020, 1, 21), 2),
+                new ClassHour("Math", new DateTime(2020, 1, 22), 6),
+                new ClassHour("English", new DateTime(2020, 1, 23), 6),
+                new ClassHour("English", new DateTime(2020, 1, 16), 1),
+            };
+
+            ReportTable table = new ReportTable(data);
+            StringBuilder sb = new StringBuilder();
+            table.Render(sb);
+            output.WriteLine(sb.ToString());
+        }
+    }
+}
