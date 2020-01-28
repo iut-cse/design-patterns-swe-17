eb48606407b72c8942cb9990f4a22cc05d9db351
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-01-28 06:33:27 +0600

    render except total row, no formatting

--- DesignPatterns/DesignPatterns/_00_Mohayemin/Reports/ReportTable.cs
+++ DesignPatterns/DesignPatterns/_00_Mohayemin/Reports/ReportTable.cs
@@ -6,68 +6,140 @@ using System.Text;
 
 namespace DesignPatterns._00_Mohayemin.Reports
 {
-    class ReportTable: IRenderable
+    class ReportTable : IRenderable
     {
-        private readonly List<ReportRow> rows;
+        private List<ReportRow> rows;
+        private List<ReportColumn> cols;
+        private ReportCell[,] cells;
 
         public ReportTable(List<ClassHour> classHours)
         {
-            var departments = classHours.ConvertAll(ch => ch.department).Distinct().ToList();
-            rows = departments.ConvertAll(d => new ReportRow(d, classHours.FindAll(ch => ch.department == d)));
+            BuildComponents(classHours);
         }
 
         public void Render(StringBuilder builder)
         {
-            foreach (var row in rows)
+            for (int r = 0; r < cells.GetLength(0); r++)
             {
-                row.Render(builder);
+                builder.Append("|");
+                for (int c = 0; c < cells.GetLength(1); c++)
+                {
+                    builder.Append(" ");
+                    cells[r, c].Render(builder);
+                    builder.Append(" |");
+                }
                 builder.Append("\n");
             }
         }
+
+        private void BuildComponents(List<ClassHour> classHours)
+        {
+            var departments = classHours.ConvertAll(ch => ch.department).Distinct().ToList();
+
+            rows = new List<ReportRow>();
+            rows.Add(new ReportRow());
+            foreach (var department in departments)
+            {
+                rows.Add(new ReportRow());
+            }
+            rows.Add(new ReportRow());
+            //rows = departments.ConvertAll(d => new ReportRow(d, classHours.FindAll(ch => ch.department == d)));
+
+
+            cols = new List<ReportColumn>();
+            cols.Add(new ReportColumn());
+
+            cells = new ReportCell[1 + departments.Count /*+ 1*/, 1 + 7 + 1];
+
+            int r = 0, c = 0;
+            cells[r, c] = new ReportCell("Department");
+            for (c++; c <= 7; c++)
+            {
+                cells[r, c] = new ReportCell(AllDaysOfWeek.FromMonday[c - 1].ToString());
+            }
+            cells[r, c] = new ReportCell("Total");
+
+            for (r++; r < departments.Count + 1; r++)
+            {
+                c = 0;
+                var department = departments[r - 1];
+                cells[r, c] = new ReportCell(department);
+                var total = 0;
+                for (c++; c <= 7; c++)
+                {
+                    var dow = AllDaysOfWeek.FromMonday[c - 1];
+                    var value = classHours.FindAll(ch => ch.date.DayOfWeek == dow && ch.department == department).Sum(ch => ch.durationHours);
+                    cells[r, c] = new ReportDataCell(value);
+                    total += 0;
+                }
+                cells[r, c] = new ReportDataCell(total);
+            }
+
+            
+        }
     }
 
-    class ReportRow: IRenderable
+    class ReportRow
     {
         private readonly ReportCell departmentCell;
         private readonly List<ReportCell> dayCells;
         private readonly ReportCell totalCell;
 
-        public ReportRow(string department, List<ClassHour> classHours)
+        public ReportRow()
         {
-            departmentCell = new ReportCell(department);
-            dayCells = new List<ReportCell>();
-            var groupedClassHours = classHours.GroupBy(ch => ch.date.DayOfWeek).ToDictionary(g => g.Key, g => g.Sum(ch => ch.durationHours));
-            foreach (var dow in AllDaysOfWeek.FromMonday)
-            {
-                var cellValue = groupedClassHours.ContainsKey(dow) ? groupedClassHours[dow] : 0;
-                dayCells.Add(new ReportCell(cellValue.ToString()));
-            }
 
-            totalCell = new ReportCell(classHours.Sum(ch => ch.durationHours).ToString());
-        }
-
-        public void Render(StringBuilder builder)
-        {
-            builder.Append("|");
-            departmentCell.Render(builder);
-            foreach (var dc in dayCells)
-                dc.Render(builder);
-
-            totalCell.Render(builder);
         }
     }
 
-    class ReportCell: IRenderable
+    class ReportColumn
     {
-        public string Content { get; }
+        private List<ReportCell> allCells;
+        private ReportCell headerCell;
+        private List<ReportDataCell> dataCells;
+        public ReportColumn()
+        {
+            allCells = new List<ReportCell>();
+        }
+
+        public void SetHeaderCell(ReportCell headerCell)
+        {
+            this.headerCell = headerCell;
+            allCells.Add(headerCell);
+        }
+
+        public void AddDataCell(ReportDataCell dataCell)
+        {
+            allCells.Add(dataCell);
+            dataCells.Add(dataCell);
+        }
+
+        public int CalculateWidth()
+        {
+            return allCells.Max(c => c.content.Length);
+        }
+    }
+
+    class ReportCell : IRenderable
+    {
+        public readonly string content;
         public ReportCell(string content)
         {
-            Content = content;
+            this.content = content;
         }
 
         public void Render(StringBuilder builder)
         {
-            builder.Append($" {Content} |");
+            builder.Append($"{content}");
+        }
+    }
+
+    class ReportDataCell : ReportCell
+    {
+        public readonly double value;
+
+        public ReportDataCell(double value) : base(value.ToString())
+        {
+            this.value = value;
         }
     }
 
