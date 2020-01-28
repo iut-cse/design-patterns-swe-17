254aa4d33fc3b94269943c92cb2e7c7b21ec1fef
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-01-28 13:59:37 +0600

    move classhour model class inside the ReportTable.cs file

--- DesignPatterns/DesignPatterns/_00_Mohayemin/Reports/ReportTable.cs
+++ DesignPatterns/DesignPatterns/_00_Mohayemin/Reports/ReportTable.cs
@@ -164,4 +164,20 @@ namespace DesignPatterns._00_Mohayemin.Reports
     {
         public static DayOfWeek[] FromMonday = new[] { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
     }
+
+    class ClassHour
+    {
+        public readonly string department;
+        public readonly DateTime date;
+        public readonly int durationHours;
+
+        public ClassHour(string department
+            , DateTime date
+            , int duration)
+        {
+            this.department = department;
+            this.date = date;
+            this.durationHours = duration;
+        }
+    }
 }
