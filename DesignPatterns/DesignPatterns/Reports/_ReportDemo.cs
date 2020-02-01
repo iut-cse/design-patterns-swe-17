e119b47d02fbb770e63940ef04aa94632e9007cb
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-02-01 12:13:45 +0600

    #3 solution for first step of tabular report

---
+++ DesignPatterns/DesignPatterns/Reports/_ReportDemo.cs
@@ -0,0 +1,29 @@
+﻿using DesignPatterns.__Commons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPatterns.Reports
+{
+    public class _ReportDemo
+    {
+        [Fact]
+        void Demo()
+        {
+            var data = new List<ClassHour>
+            {
+                new ClassHour("Physics", new DateTime(2020, 1, 20), 4),
+                new ClassHour("Physics", new DateTime(2020, 1, 27), 5),
+                new ClassHour("Chemistry", new DateTime(2020, 1, 21), 2),
+                new ClassHour("Math", new DateTime(2020, 1, 22), 6),
+                new ClassHour("English", new DateTime(2020, 1, 23), 6),
+                new ClassHour("English", new DateTime(2020, 1, 16), 1),
+            };
+            ReportTable table = new ReportTable(data);
+            StringBuilder sb = new StringBuilder();
+            table.Render(sb);
+            new OutputFile().Write(sb.ToString());
+        }
+    }
+}
