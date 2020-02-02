97707edb2d8a83daa7d127699038f9e853ed4aa6
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-02-02 11:42:18 +0600

    #2 task modified

--- 36-Riyad/src/lab1/ShowScore.java
+++ 36-Riyad/src/lab1/ShowScore.java
@@ -10,20 +10,20 @@ public class ShowScore {
 		
 		System.out.println("Enter type of game: 1.Football 2.Cricket");
 		int number = input.nextInt();
-				
+		
+		Game game = null;
 		
 		if(number ==1) {
-			Football ft = new Football("Gryffindor vs Hufflepuff","2 - 3","Newt of Hufflepuff");
-			ft.teamInfo();
-			ft.score();
-			ft.bestPlayer();
+			game = new Football("Gryffindor vs Hufflepuff","2 - 3","Newt of Hufflepuff");
+			
 		}
 		else if(number == 2){
-			Cricket ct = new Cricket("Ravenclaw vs Slytherin","T20","121/6 (16.1) - 122/7 (18.4)","Luna of Ravenclaw","Regulus of Slytherin");
-			ct.teamInfo();
-			ct.score();
-			ct.bestPlayer();
+			game = new Cricket("Ravenclaw vs Slytherin","T20","121/6 (16.1) - 122/7 (18.4)","Luna of Ravenclaw","Regulus of Slytherin");
+			
 		}
+		game.teamInfo();
+		game.score();
+		game.bestPlayer();
 		
 	
 		
