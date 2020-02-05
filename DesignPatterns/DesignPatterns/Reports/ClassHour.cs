5e1ccae4b7feb573254a670d6aa6cc5548d07ff0
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-02-05 07:16:57 +0600

    add paymentBdt field in ClassHour class

--- DesignPatterns/DesignPatterns/Reports/ClassHour.cs
+++ DesignPatterns/DesignPatterns/Reports/ClassHour.cs
@@ -7,14 +7,17 @@ namespace DesignPatterns.Reports
         public readonly string department;
         public readonly DateTime date;
         public readonly double durationHours;
+        public readonly double paymentBdt;
 
         public ClassHour(string department
             , DateTime date
-            , double durationHours)
+            , double durationHours
+            , double paymentBdt = 0)
         {
             this.department = department;
             this.date = date;
             this.durationHours = durationHours;
+            this.paymentBdt = paymentBdt;
         }
     }
 }
