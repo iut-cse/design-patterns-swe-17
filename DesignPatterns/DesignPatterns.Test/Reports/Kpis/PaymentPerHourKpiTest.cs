7be114e4e0ddb1925116fe06a73b00e34feefbdb
User <User@DESKTOP-04BCER1>
2020-02-12 11:29:57 +0600

    payment per hour kpi and test created

---
+++ DesignPatterns/DesignPatterns.Test/Reports/Kpis/PaymentPerHourKpiTest.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatterns.Reports.Kpis;
+using System;
+using Xunit;
+
+namespace DesignPatterns.Test.Reports.Kpis
+{
+    public class PaymentPerHourKpiTest
+    {
+        [Fact]
+        public void Monday()
+        {
+            var data = TestData.CreateData();
+            var tch = new TotalPaymentKpi(data, DayOfWeek.Monday);
+            var output = tch.Calculate();
+
+            Assert.Equal(5200, output["Physics"]);
+            Assert.Equal(2100, output["Bangla"]);
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Reports/Kpis/PaymentPerHourKpi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Reports.Kpis
+{
+    class PaymentPerHourKpi:Kpi
+    {
+       
+         public PaymentPerHourKpi(IEnumerable<ClassInfo> classHours , DayOfWeek dayOfWeek)
+            :base (classHours, dayOfWeek)
+        {
+           
+        }
+        public KpiResult Calculate(){
+        }
+     
+    }
+
+
+
+}
