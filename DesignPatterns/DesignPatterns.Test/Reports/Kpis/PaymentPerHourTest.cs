8cb274279d13c126ab92014eaede6bceaea02d49
CSE <CSE@CSE-L2-PC-02>
2020-02-12 12:15:49 +0600

    Task 2 Done

--- DesignPatterns/DesignPatterns.Test/Reports/Kpis/PaymentPerHourTest.cs
+++ DesignPatterns/DesignPatterns.Test/Reports/Kpis/PaymentPerHourTest.cs
@@ -4,16 +4,19 @@ using Xunit;
 
 namespace DesignPatterns.Test.Reports.Kpis
 {
-    public class TotalPaymentKpiTest
+    public class PaymentPerHourTest
     {
+
         [Fact]
         public void Monday()
         {
             var data = TestData.CreateData();
-            var tch = new TotalPaymentKpi(data, DayOfWeek.Monday);
+            var tch = new PaymentPerHour(data, DayOfWeek.Monday);
             var output = tch.Calculate();
-            Assert.Equal(5200, output["Physics"]);
-            Assert.Equal(2100, output["Bangla"]);
+
+            Assert.Equal((5200.0/9), output["Physics"]);
+            Assert.Equal((2100.0/6), output["Bangla"]);
         }
+
     }
 }
---
+++ DesignPatterns/DesignPatterns/Reports/Kpis/PaymentPerHour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DesignPatterns.Reports.Kpis
+{
+    public  class PaymentPerHour : Kpi
+    {
+
+        public PaymentPerHour(IEnumerable<ClassInfo> classHours
+             , DayOfWeek dayOfWeek)
+             : base(classHours, dayOfWeek)
+        {
+        }
+
+        protected override IDictionary<string, double> GroupToDictionary(IEnumerable<IGrouping<string, ClassInfo>> grouped)
+        {
+            return grouped.ToDictionary(g => g.Key, g => g.Sum(ch => ch.paymentBdt)/ g.Sum(ch => ch.durationHours));
+        }
+
+
+
+    }
+}
