020dc41778730981044c26edb494c876c8b2e48d
CSE <CSE@CSE-L2-PC-13>
2020-02-12 12:15:32 +0600

    Added PerHourPayment and test

---
+++ DesignPatterns/DesignPatterns.Test/Reports/Kpis/PaymentperHourTest.cs
@@ -0,0 +1,35 @@
+﻿using DesignPatterns.Reports;
+using DesignPatterns.Reports.Kpis;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace DesignPatterns.Test.Reports.Kpis
+{
+    public class PaymentperHourTest
+    {
+        [Fact]
+        public void Monday()
+        {
+            var data = TestData.CreateData();
+            var tch = new PaymentperHour(data, DayOfWeek.Monday);
+            var output = tch.Calculate();
+
+            Assert.Equal(5200.0/9, output["Physics"]);
+            Assert.Equal(2100.0/6, output["Bangla"]);
+        }
+
+
+        [Fact]
+        public void Tuesday()
+        {
+            var data = TestData.CreateData();
+            var tch = new PaymentperHour(data, DayOfWeek.Tuesday);
+            var output = tch.Calculate();
+
+            Assert.Equal(400.0/5, output["Physics"]);
+            Assert.Equal(100.0/2, output["Chemistry"]);
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Reports/Kpis/PaymentperHour.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.Reports;
+using DesignPatterns.Reports.Kpis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PaymentperHour : Kpi
+{
+    public PaymentperHour(IEnumerable<ClassInfo> classHours, DayOfWeek dayOfWeek) : base(classHours, dayOfWeek)
+    {
+    }
+
+    protected override IDictionary<string, double> GroupToDictionary(IEnumerable<IGrouping<string, ClassInfo>> grouped)
+    {
+        return grouped.ToDictionary(g => g.Key, g => (g.Sum(ch => ch.paymentBdt)/g.Sum(ch => ch.durationHours)));
+    }
+}
