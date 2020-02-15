f4f95e932bcf819ffbb5536949bdc128b51c4838
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-02-15 19:27:45 +0600

    lazy initialize the instance. This is often desired that the instance is created only when needed for the first time. This is specially needed if the instance is resource hungry.

--- DesignPatterns/DesignPatterns/Reports/ReportConfig.cs
+++ DesignPatterns/DesignPatterns/Reports/ReportConfig.cs
@@ -2,9 +2,19 @@
 {
     public class ReportConfig
     {
-        public static readonly ReportConfig Instance = new ReportConfig();
+        private static ReportConfig instance;
+
+        public static ReportConfig GetInstance()
+        {
+            if (instance == null) 
+                instance = new ReportConfig();
+
+            return instance;
+        }
 
         private ReportConfig() { }
+
+        
 
         public string ColumnSeparator { get; set; }
     }
