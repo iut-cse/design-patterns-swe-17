2ac101f3ba37249e17204057ccdf71139815b136
Md. Ariful Islam, 170042040 <arifulislam40@iut-dhaka.edu>
2020-02-19 10:09:40 +0600

    ...

---
+++ DesignPatterns/DesignPatterns/Reports/Kpis/PaymentPerHour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Reports.Kpis
+{
+    public class PaymentPerHour : Kpi
+    {
+        public PaymentPerHour(IEnumerable<ClassInfo> classHours
+            , DayOfWeek dayOfWeek)
+            : base(classHours, dayOfWeek)
+        {
+        }
+
+        protected override IDictionary<string, double> GroupToDictionary(IEnumerable<IGrouping<string, ClassInfo>> grouped)
+        {
+            return grouped.ToDictionary(g => g.Key, g => g.Sum(ci => ci.paymentBdt) / g.Sum(ci => ci.durationHours));
+        }
+
+    }
+}
