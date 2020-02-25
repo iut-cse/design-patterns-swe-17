9512a240b50cb98aa42dbd42f0594fd5403e90ae
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-02-25 11:38:12 +0600

    use test data in report demo

--- DesignPatterns/DesignPatterns.Test/_Demos/ReportDemo.cs
+++ DesignPatterns/DesignPatterns.Test/_Demos/ReportDemo.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using DesignPatterns.Test.Reports.Kpis;
 using Xunit;
 
 namespace DesignPatterns.Test._Demos
@@ -11,17 +12,9 @@ namespace DesignPatterns.Test._Demos
         [Fact]
         void Demo()
         {
-            var data = new List<ClassInfo>
-            {
-                new ClassInfo("Physics", new DateTime(2020, 1, 20), 4),
-                new ClassInfo("Physics", new DateTime(2020, 1, 27), 5),
-                new ClassInfo("Chemistry", new DateTime(2020, 1, 21), 2),
-                new ClassInfo("Math", new DateTime(2020, 1, 22), 6),
-                new ClassInfo("English", new DateTime(2020, 1, 23), 6),
-                new ClassInfo("English", new DateTime(2020, 1, 16), 1),
-            };
-            ReportTable table = new ReportTable(data, "left");
-            StringBuilder sb = new StringBuilder();
+            var data = TestData.CreateData();
+            var table = new ReportTable(data, "left");
+            var sb = new StringBuilder();
             table.Render(sb);
             new OutputFile().Write(sb.ToString());
         }
