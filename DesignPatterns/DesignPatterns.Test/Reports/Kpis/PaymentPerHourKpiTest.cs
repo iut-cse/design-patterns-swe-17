ec71a7999715922068fb2c732d110c8e9b06d43a
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-02-25 11:19:23 +0600

    test PaymentPerHour KPI

---
+++ DesignPatterns/DesignPatterns.Test/Reports/Kpis/PaymentPerHourKpiTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DesignPatterns.Reports.Kpis;
+using Xunit;
+
+namespace DesignPatterns.Test.Reports.Kpis
+{
+    public class PaymentPerHourKpiTest
+    {
+        [Fact]
+        public void Monday()
+        {
+            var data = TestData.CreateData();
+            var tch = new PaymentPerHourKpi(data, DayOfWeek.Monday);
+            var output = tch.Calculate();
+
+            Assert.Equal(5200.0/9, output["Physics"], 4);
+            Assert.Equal(2100.0/6, output["Bangla"], 4);
+        }
+    }
+}
