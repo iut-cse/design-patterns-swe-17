b4bf9db43118970805f262578ddf50dbd756b0da
Md. Ariful Islam, 170042040 <arifulislam40@iut-dhaka.edu>
2020-03-10 22:43:08 +0600

    ...

---
+++ 40-arif/DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
+{
+    public class Preference
+    {
+        private static Preference instance;
+        private List<string> list;
+
+        
+        private Preference()
+        {
+
+        }
+        public static Preference getInstance()
+        {
+            if (instance == null)
+            {
+                //acquireThreadLock();
+                if (instance == null)
+                {
+                    instance = new Preference();
+                }
+            }
+            return instance;
+        }
+
+
+        public void setPreference(string key, string val)
+        {
+            
+        }
+
+        public string getPreference(string key)
+        {
+            return "get";
+        }
+    }
+}
