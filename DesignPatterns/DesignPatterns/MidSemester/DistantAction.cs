8cb43b03258fd0cdb57b475a29ae4a7b43820244
Iftekharul Abedeen <iftekharul@iut-dhaka.edu>
2020-03-10 22:59:57 +0600

    MidSemester Task 1 complete

---
+++ DesignPatterns/DesignPatterns/MidSemester/DistantAction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
+{
+    public abstract class DistantAction : IDistantAction
+    {
+        public abstract string Grab();
+
+        public abstract string Hit();
+
+        public string Move()
+        {
+            return "move";
+        }
+
+        public IEnumerable<string> TakeDistantAction()
+        {
+            yield return this.Move();
+            yield return this.Grab();
+            yield return this.Hit();
+        }
+    }
+
+    public class Power : DistantAction
+    {
+        public override string Grab()
+        {
+            return "pick up";
+        }
+
+        public override string Hit()
+        {
+            return "slam!";
+        }
+    }
+
+    public class Skill : DistantAction
+    {
+        public override string Grab()
+        {
+            return "hold collar";
+        }
+
+        public override string Hit()
+        {
+            return "knee! knee!! knee!!!";
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/MidSemester/ICloseAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester
+{
+    public interface ICloseAction
+    {
+        public string TakeCloseAction();
+    }
+
+    public class Punch : ICloseAction
+    {
+        public string TakeCloseAction()
+        {
+            return "punch";
+        }
+    }
+
+    public class Kick : ICloseAction
+    {
+        public string TakeCloseAction()
+        {
+            return "kick";
+        }
+    }
+
+    public class Head : ICloseAction
+    {
+        public string TakeCloseAction()
+        {
+            return "head head";
+        }
+    }
+}
