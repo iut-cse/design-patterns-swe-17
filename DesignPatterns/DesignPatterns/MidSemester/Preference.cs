733a41a6c621addac3622f5762b2523610c64497
Iftekharul Abedeen <iftekharul@iut-dhaka.edu>
2020-03-11 03:19:52 +0600

    MidSEmester task 3 complete

--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,7 +1,49 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
-        // TODO: implement
+        private static Preference instance;
+        private static readonly object ThreadLock = new object();
+        private Dictionary<string, string> dicton = new Dictionary<string, string>();
+
+        private Preference()
+        {
+
+        }
+
+        public static Preference GetInstance()
+        {
+            if(instance == null)
+            {
+                lock (ThreadLock)
+                {
+                    if(instance == null)
+                    {
+                        instance = new Preference();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public void setPreference(string key, string value)
+        {
+            if(!dicton.ContainsKey(key))
+            {
+                dicton.Add(key, value);
+            }
+
+            else
+            {
+                dicton[key] = value;
+            }
+        }
+
+        public string getPreference(string key)
+        {
+            return dicton[key];
+        }
     }
 }
