51c7892c3f47f751b2cbdfe514c15485a602bc5c
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-03-11 15:19:12 +0600

    test push

--- DesignPatterns/DesignPatterns/MidSemester/powerAction.cs
+++ DesignPatterns/DesignPatterns/MidSemester/powerAction.cs
@@ -1,14 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+namespace DesignPatterns.MidSemester
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    class powerAction
+    {
+    }
 }
+
