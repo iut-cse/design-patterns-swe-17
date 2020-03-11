2f2e3e10b6f1f86d6eae0ff2fddc3cb513c1b158
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-03-11 13:08:11 +0600

    Mid Problems solved 1, 2, 3

---
+++ DesignPatterns/DesignPatterns/MidSemester/Head.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.MidSemester
+{
+    public class Head : CloseAction
+    {
+        public string action()
+        {
+            return "head head";
+        }
+    }
+}
+
+
--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,7 +1,44 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
         // TODO: implement
+        private static Preference instance;
+        private static readonly object padlock = new object();
+
+        private Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        private Preference() { }
+        public static Preference getInstance()
+        {
+            if(instance == null)
+            {
+                lock(padlock)
+                {
+                    if(instance == null)
+                    {
+                        instance = new Preference();
+                    }
+                }
+            }
+            return instance;
+        }
+        public void setPreference(string key, string value)
+        {
+            if (!dict.ContainsKey(key))
+            { 
+                dict.Add(key, value);
+            }
+            else
+            {
+                dict[key] = value;
+            }
+        }
+        public string getPreference(string key)
+        {
+            return dict[key];
+        }
     }
 }
