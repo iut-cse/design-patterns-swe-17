abccabde09edc795a5f409fa8827a9b1fe9e3c57
XLR8-07 <joyashikur@gmail.com>
2020-03-12 22:56:59 +0600

    Class Model Implemented for Decorator Pattern to solve MidSemTask 2

---
+++ DesignPatterns/DesignPatterns/MidSemester/EncryptionDecorator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester
+{
+    public class EncryptionDecorator : BaseDecorator
+    {
+        public EncryptionDecorator(IStatistics statistics) : base(statistics)
+        {
+        }
+
+        public override string statistics(string data)
+        {
+            return wrappee.statistics(data).ToLower();
+        }
+    }
+}
