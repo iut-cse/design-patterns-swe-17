a3a137eb1e879deefafe6b4545a9668fe21f1c69
XLR8-07 <joyashikur@gmail.com>
2020-03-12 23:02:32 +0600

    Compression Decorator Implemented

--- DesignPatterns/DesignPatterns/MidSemester/CompressionDecorator.cs
+++ DesignPatterns/DesignPatterns/MidSemester/CompressionDecorator.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace DesignPatterns.MidSemester
 {
-    class CompressionDecorator
+    public class CompressionDecorator : BaseDecorator
     {
+        public CompressionDecorator(IStatistics statistics) : base(statistics)
+        {
+
+        }
+        public override string statistics(string data)
+        {
+            if (wrappee.statistics(data).Length >= 3)
+                return wrappee.statistics(data).Substring(0, wrappee.statistics(data).Length - 2);
+            else
+                return wrappee.statistics(data);
+        }
     }
 }
