28a4d5e4535fae48933dbf315062a8512419ba12
rizwan19 <rizwanulhaque@iut-dhaka.edu>
2020-03-13 03:15:51 +0600

    created instance1 and instance2

--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,7 +1,29 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
         // TODO: implement
+        private static Preference instance;
+        private IDictionary<string, string> dict = new Dictionary<string, string>();
+        private Preference()
+        {
+
+        }
+        public void setPreference(string key, string value)
+        {
+            dict[key] = value;
+        }
+        public string getPreference(string key)
+        {
+            return dict[key];
+        }
+        public static Preference GetInstance()
+        {
+            if (instance == null)
+                instance = new Preference();
+            return instance;
+        }
     }
 }
