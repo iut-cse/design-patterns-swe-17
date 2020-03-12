3212f26bf2ac042435e27d41bb77d37c5ba4f9b8
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-03-13 00:17:29 +0600

    #midProblem3

---
+++ 36-Riyad/DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
@@ -0,0 +1,23 @@
+﻿using DesignPatterns.MidSemester;
+using Xunit;
+
+namespace DesignPatterns.Test.MidSemester
+{
+    public class MidProblem3Test
+    {
+        [Fact]
+        void DoTest()
+        {
+            Preference instance1 = Preference.getObject(); // initialize correctly
+            // set value "123" with key "abc"
+            string abcValue = "123"; // get with key "abc";
+            instance1.setPreference("abc", "123");
+            Assert.Equal(abcValue, instance1.getPreference("abc"));
+
+            Preference instance2 = Preference.getObject();// initialize correctly
+            string abcValue2 = "567";// set value "567" with key "abc" in instance2.
+            instance2.setPreference("abc", "567");// get value with key "abc" from instance1.
+            Assert.Equal(abcValue2, instance2.getPreference("abc"));// assert
+        }
+    }
+}
---
+++ 36-Riyad/DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
+{
+    public class Preference
+    {
+        private static Preference instance;
+        string key, value;
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+
+
+        private Preference() { }
+
+        public static Preference getObject()
+        {
+            if(instance == null)
+            {
+                instance = new Preference();
+            }
+
+            return instance;
+        }
+
+        public void setPreference(string key, string value)
+        {
+            dict[key] = value;
+        }
+
+        public string getPreference(string key)
+        {
+            return dict[key];
+        }
+    }
+}
