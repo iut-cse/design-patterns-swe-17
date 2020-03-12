6eeb17ae52f81109ca0e1bfbf0084097fff960d4
XLR8-07 <joyashikur@gmail.com>
2020-03-12 23:56:58 +0600

    Singleton Imeplemented for Preference and MidSemTask 3 Solved with Unit Tests

--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,7 +1,44 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
-        // TODO: implement
+        private static Preference preference;
+        private Dictionary<string,string> dictionary = new Dictionary<string, string>();
+        private static readonly object Thread = new object(); //skipping the hassle of ReadWriterLockSlim
+
+        private Preference()
+        {}
+
+        public static Preference Instantiate()
+        {
+            if(preference == null)
+            {
+                lock(Thread)
+                {
+                    if(preference == null)
+                    {
+                        preference = new Preference();
+                    }
+                }
+            }
+            return preference;
+        }
+
+        public void setPreference(string key , string value)
+        {
+            if (!(dictionary.ContainsKey(key)))
+            {
+                dictionary.Add(key, value);
+            }
+            else
+                dictionary[key] = value;
+        }
+
+        public string getPreference(string key)
+        {
+            return dictionary[key];
+        }
     }
 }
