3f18c526b14f96887222994053525982288e3c11
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-03-14 02:44:18 +0600

    mid_solve2

--- 44-rafsan/DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ 44-rafsan/DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -2,6 +2,37 @@
 {
     public class Preference
     {
-        // TODO: implement
+        private static Preference preference = null;
+        private static readonly object padlock = new object();
+        string key;
+        string value;
+        private Preference()
+        {
+        }
+        public static Preference getInstance()
+        {
+            if (preference == null)
+            {
+                lock (padlock)
+                {
+                    if (preference == null)
+                    {
+                        preference = new Preference();
+                    }
+                }
+            }
+
+
+            return preference;
+        }
+        public void SetPreference(string value, string key)
+        {
+            this.key = key;
+            this.value = value;
+        }
+        public string GetPreference(string key)
+        {
+            return value;
+        }
     }
 }
---
+++ 44-rafsan/DesignPatterns/DesignPatterns/MidSemesterTests/Mid_problemTestCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DesignPatterns.MidSemester;
+using Xunit;
+namespace DesignPatterns.MidSemesterTests
+{
+    public class Mid_problemTestCase
+    {
+        [Fact]
+        void DoTest()
+        {
+            Preference case1 = Preference.getInstance(); 
+            case1.SetPreference("170042044", "rafsan");
+            string abcValue = case1.GetPreference("rafsan");
+            Assert.Equal("170042044", abcValue);
+
+          
+            Preference case2 = Preference.getInstance();
+            case2.SetPreference("1700420100", "rafsan");
+            string Value = case1.GetPreference("rafsan");
+            Assert.Equal("1700420100", Value);
+        }
+    }
+}
+}
