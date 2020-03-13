2ee6fafb15cf83f2039c8d0e1cc0fd69568c2b6b
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-03-13 16:33:53 +0600

    Added the interface.

---
+++ DesignPatterns/DesignPatterns/MidSemester/I_CloseAction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester
+{
+    public interface I_CloseAction
+    {
+        string DoAction();
+    }
+}
