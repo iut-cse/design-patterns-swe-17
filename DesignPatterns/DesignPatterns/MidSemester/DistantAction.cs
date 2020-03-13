311afe69230c0ab0b51e01d326c50645ae5593bf
30-jubair <jobairt03@gmail.com>
2020-03-14 05:33:00 +0600

    Task1 Completed

---
+++ DesignPatterns/DesignPatterns/MidSemester/DistantAction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester
+{
+
+        public abstract class DistantAction
+        {
+            public IEnumerable<String> PerformDistantaction()
+            {
+                yield return Move();
+                yield return Grab();
+                yield return Hit();
+            }
+            private String Move()
+            {
+                return "move";    // Concrete Method
+            }
+
+            abstract public String Grab();
+            abstract public String Hit();
+        }
+
+        public class PowerAction : DistantAction
+        {
+            public override string Grab()
+            {
+                return "pick up";
+            }
+
+            public override string Hit()
+            {
+                return "slam";
+            }
+        }
+
+        public class SkillDAction : DistantAction
+        {
+            public override string Grab()
+            {
+                return "hold collar";
+            }
+
+            public override string Hit()
+            {
+                return "knee! knee!! knee!!!";
+            }
+        }
+
+    }
+
--- DesignPatterns/DesignPatterns/MidSemester/Ratul.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Ratul.cs
@@ -4,18 +4,18 @@ namespace DesignPatterns.MidSemester
 {
     public class Ratul
     {
-        public string ExecuteCloseAction()
+        public string ExecuteCloseAction(ICloseAction closeAction)
         {
-            return null;
+            return closeAction.performCloseAction()
         }
 
-        public IEnumerable<string> ExecuteDistantAction()
+        public IEnumerable<string> ExecuteDistantAction(DistantAction distantAction)
         {
             // you can return a array or list of string. Both array and list are subtypes of IEnumerable
             // However, there is a thing called `yield return` in C#.
             // Using it would be "cool"
             // Make sure you understand it if you choose to use it.
-            return null;
+            return distantAction.PerformDistantaction();
         }
     }
 }
