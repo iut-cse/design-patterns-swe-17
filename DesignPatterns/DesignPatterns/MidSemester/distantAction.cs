8c74a1d632edc6f44adf041910475e638a0bdeb9
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-03-13 15:13:04 +0600

    Distant Actiob Added

--- DesignPatterns/DesignPatterns/MidSemester/distantAction.cs
+++ DesignPatterns/DesignPatterns/MidSemester/distantAction.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace DesignPatterns.MidSemester
 {
-    class distantAction
+    public abstract class distantAction
     {
+        public List<string> actionList = new List<string>();
+
+        public IEnumerable<string> AllDistantAction()
+        {
+            actionList.Add(Move());
+            actionList.Add(Grab());
+            actionList.Add(Hit());
+
+            return actionList;
+        }
+        public string Move()
+        {
+            return "move";
+        }
+
+        public abstract string Grab();
+        public abstract string Hit();
     }
 }
