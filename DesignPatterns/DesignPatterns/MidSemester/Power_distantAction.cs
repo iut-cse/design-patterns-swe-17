2c0ee90f8ba78aec620654bb13b00cc6c425b75c
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-03-13 16:38:08 +0600

    added two abstract class that implements the distantAction class.

---
+++ DesignPatterns/DesignPatterns/MidSemester/Power_distantAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester
+{
+    class Power_distantAction : distantAction
+    {
+        public override string Grab()
+        {
+            return "Pick up";
+        }
+
+        public override string Hit()
+        {
+            return "Slam";
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/MidSemester/Skill_distantAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester
+{
+    class Skill_distantAction : distantAction
+    {
+        public override string Grab()
+        {
+            return "Hold collar!";
+        }
+
+        public override string Hit()
+        {
+            return "Hit knee!";
+        }
+    }
+}
