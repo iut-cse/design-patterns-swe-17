bed8fb6695eae85ddb40a69a20a93f5d785c30ac
saimulhaque@iut-dhaka.edu <saimulhaque@iut-dhaka.edu>
2020-03-13 11:45:26 +0600

    implement setPreference(), getPreference() method and check only one instance is created

--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,7 +1,36 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
+    //using System;
     public class Preference
     {
-        // TODO: implement
+        private Preference() { }
+
+        private static Preference instance;
+
+
+        private static readonly object _lock = new object();
+        private IDictionary<string, string> dic = new Dictionary<string, string>();
+
+        public static Preference GetInstance()
+        {
+
+            if (instance == null)
+            {
+                instance = new Preference();
+            }
+            return instance;
+        }
+
+
+        public void setPreference(string key, string value)
+        {
+            dic[key] = value;
+        }
+        public string getPreference(string key)
+        {
+            return dic[key]; 
+        }
     }
 }
