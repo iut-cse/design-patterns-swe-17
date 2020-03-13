112ede3684c0f4f396b70ed1802a2fce9125f2bb
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-03-13 17:08:22 +0600

    Problem 3 is solved.

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
@@ -1,4 +1,5 @@
-﻿using DesignPatterns.MidSemester;
+﻿
+using DesignPatterns.MidSemester;
 using Xunit;
 
 namespace DesignPatterns.Test.MidSemester
@@ -8,15 +9,21 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void DoTest()
         {
-            Preference instance1 = null; // initialize correctly
+            // initialize correctly
+            Preference instance1 = Preference.GetInstance();
             // set value "123" with key "abc"
-            string abcValue = null; // get with key "abc";
+            instance1.SetPreference("abc", "123");
+            // get with key "abc"
+            string abcValue = instance1.GetPreference("abc");
             Assert.Equal("123", abcValue);
 
-            Preference instance2 = null; // initialize correctly
+            Preference instance2 = Preference.GetInstance(); // initialize correctly
             // set value "567" with key "abc" in instance2.
+            instance2.SetPreference("abc", "567");
             // get value with key "abc" from instance1.
+            string _abcvalue = instance1.GetPreference("abc");
             // assert
+            Assert.Equal("567", _abcvalue);
         }
     }
 }
--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -2,6 +2,40 @@
 {
     public class Preference
     {
-        // TODO: implement
+        private string key = null, value = null;
+        private static Preference _preference;
+        private static readonly object _lock = new object();
+        private Preference()
+        {
+
+        }
+
+        public static Preference GetInstance()
+        {
+            if (_preference == null)
+                lock (_lock)
+                    if (_preference == null)
+                        _preference = new Preference();
+
+            return _preference;
+        }
+
+        public string GetPreference(string key)
+        {
+            if (key == _preference.key)
+                return _preference.value;
+            return null;
+        }
+
+        public void SetPreference(string key, string value)
+        {
+            if (key == _preference.key)
+                _preference.value = value;
+            if (_preference.key == null)
+            {
+                _preference.key = key;
+                _preference.value = value;
+            }
+        }
     }
 }
