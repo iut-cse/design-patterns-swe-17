9b6494122fbb8177f699c12b8ed1134a91fe4126
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-03-15 02:34:26 +0600

    #3 Task 3 completed

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem2Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem2Test.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using DesignPatterns.MidSemester;
+using Xunit;
 
 namespace DesignPatterns.Test.MidSemester
 {
@@ -8,7 +9,8 @@ namespace DesignPatterns.Test.MidSemester
         void NoChange()
         {
             var original = "Top Score is 305";
-            var converted = ""; // Do the "No Change" conversion.
+            IStatistics statistics = new PlainText();
+            var converted = statistics.conversion(original);
             Assert.Equal("Top Score is 305", converted);
         }
 
@@ -16,7 +18,10 @@ namespace DesignPatterns.Test.MidSemester
         void CompressThenEncrypt()
         {
             var original = "Top Score is 305";
-            var converted = ""; // compress then encryppt.
+            IStatistics statistics = new PlainText();
+            statistics = new Compressor(statistics);
+            statistics = new Encryptor(statistics);
+            var converted = statistics.conversion(original);             
             Assert.Equal("top score is 3", converted);
         }
 
@@ -24,15 +29,25 @@ namespace DesignPatterns.Test.MidSemester
         void EncryptThenCompressThenEncodeThenCompress()
         {
             var original = "Top Score is 305";
-            var converted = ""; // do the convertion
-            Assert.Equal("(top score is ", converted);
+            IStatistics stat = new PlainText();
+            stat = new Encryptor(stat);
+            stat = new Compressor(stat);
+            stat = new base64encoder(stat);
+            stat = new Compressor(stat);
+            var converted = stat.conversion(original);
+            Assert.Equal("(top score is )", converted);
         }
 
         [Fact]
         void CompressThenEncodeThenEncrypt()
         {
-            // TODO: Implement this;
-            Assert.True(false);
+            var original = "Top Score is 305";
+            IStatistics stat = new PlainText();
+            stat = new Compressor(stat);
+            stat = new base64encoder(stat);
+            stat = new Encryptor(stat);
+            var converted = stat.conversion(original);
+            Assert.Equal("(top score is 3)", converted);            
         }
     }
 }
---
+++ DesignPatterns/DesignPatterns/MidSemester/Statistics.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester
+{
+    public interface IStatistics
+    {
+        public string conversion(string text);
+
+    }
+
+    public class PlainText : IStatistics
+    {
+        public string conversion(string text)
+        {
+            return text;
+        }
+    }
+    public class StatisticsDecorator : IStatistics
+    {
+        IStatistics statistics;
+        public StatisticsDecorator(IStatistics statistics)
+        {
+            this.statistics = statistics;
+        }
+        public virtual string conversion(string text)
+        {
+            return statistics.conversion(text);
+        }
+    }
+    public class Encryptor : StatisticsDecorator
+    {
+        public Encryptor(IStatistics statistics) : base(statistics)
+        {
+        }
+        public override string conversion(string text)
+        {
+            return base.conversion(text).ToLower();
+        }
+    }
+
+    public class Compressor : StatisticsDecorator
+    {
+        public Compressor(IStatistics statistics) : base(statistics)
+        {
+        }
+        public override string conversion(string text)
+        {
+            if (text.Length < 3)
+            {
+                return base.conversion(text);
+            }
+            else
+            {
+                return base.conversion(text).Substring(0, text.Length - 1);
+            }
+
+        }
+    }
+    public class base64encoder : StatisticsDecorator
+    {
+        public base64encoder(IStatistics statistics) : base(statistics)
+        {
+        }
+        public override string conversion(string text)
+        {
+            return "(" + base.conversion(text) + ")";
+        }
+    }
+}
