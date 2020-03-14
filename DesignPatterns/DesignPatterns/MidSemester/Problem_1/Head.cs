642390d60297198690cd921776ec3355ea605e5f
FZProt <fjprottyasha0806@gmail.com>
2020-03-14 22:20:30 +0600

    MidSemester and task folder added.

---
+++ DesignPatterns/DesignPatterns/MidSemester/Problem_1/Head.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.MidSemester.Problem_1
+{
+    public class Head : CloseAction
+    {
+        public string ExecuteCloseAction()
+        {
+            return "head head";
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/MidSemester/Problem_1/Punch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.MidSemester.Problem_1
+{
+    public class Punch : CloseAction
+    {
+        public string ExecuteCloseAction()
+        {
+            return "punch";
+        }
+    }
+}
