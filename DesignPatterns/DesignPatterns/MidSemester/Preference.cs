ccb6c2c3194d93e90b60775c7235428430d8c742
FZProt <fjprottyasha0806@gmail.com>
2020-03-15 01:34:05 +0600

    task 3 done.

--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,17 +1,46 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
+        IDictionary<string, string> keyVal = new Dictionary<string, string>();
         // TODO: implement
 
-        void setPreference(string key, string value)
-        {
+        private static Preference instance;
+        private static readonly object acquireLock = new object();
 
+        private Preference()
+        {
         }
 
-        string getPreference(string key)
+        public static Preference getInstance()
         {
-            return null;
+            if (instance == null)
+            {
+                lock (acquireLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Preference();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public void setPreference(string key, string value)
+        {
+            if (keyVal.ContainsKey(key))
+            {
+                keyVal.Remove(key);
+            }
+            keyVal.Add(key, value);
+        }
+
+        public string getPreference(string key)
+        {
+            return keyVal[key];
         }
 
     }
