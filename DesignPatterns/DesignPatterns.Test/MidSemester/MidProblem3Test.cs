83f801868f1b90198691fec2627d6c0d49e5c619
86-Ashik <nusayerashik@iut-dhaka.edu>
2020-03-14 22:57:20 +0600

    Task 1 done

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
@@ -10,14 +10,17 @@ namespace DesignPatterns.Test.MidSemester
         {
             Preference instance1 = Preference.getInstance(); // initialize correctly
             // set value "123" with key "abc"
-            instance1.setPreferance("123", "abc");
+            instance1.setPreferance("abc", "123");
             string abcValue = instance1.getPreferance("abc"); // get with key "abc";
             Assert.Equal("123", abcValue);
 
-            Preference instance2 = Preference.setPreferance(); // initialize correctly
+            Preference instance2 = Preference.getInstance(); // initialize correctly
             // set value "567" with key "abc" in instance2.
+            instance2.setPreferance("abc", "567");
             // get value with key "abc" from instance1.
+            string abcValue2 = instance1.getPreferance("abc");
             // assert
+            Assert.Equal("567", abcValue2);
         }
     }
 }
--- DesignPatterns/DesignPatterns/MidSemester/Ratul.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Ratul.cs
@@ -4,9 +4,22 @@ namespace DesignPatterns.MidSemester
 {
     public class Ratul
     {
+        private ICloseAction closeAction;
+        private IDistinctAction distinctAction;
+
+        public Ratul(ICloseAction closeAction)
+        {
+            this.closeAction = closeAction;
+        }
+
+        public Ratul(IDistinctAction distinctAction)
+        {
+            this.distinctAction = distinctAction;
+        }
+
         public string ExecuteCloseAction()
         {
-            return null;
+            return closeAction.execute();
         }
 
         public IEnumerable<string> ExecuteDistantAction()
@@ -15,7 +28,102 @@ namespace DesignPatterns.MidSemester
             // However, there is a thing called `yield return` in C#.
             // Using it would be "cool"
             // Make sure you understand it if you choose to use it.
-            return null;
+            return distinctAction.execute();
         }
     }
+
+    public interface ICloseAction
+    {
+        public string execute();
+    }
+
+    public abstract class IDistinctAction
+    {
+        public abstract string grab();
+        public abstract string hit();
+
+        public string move()
+        {
+            return "move";
+        }
+        public abstract List<string> execute();
+    }
+
+    public class Punch : ICloseAction
+    {
+        public string execute()
+        {
+            return "punch";
+        }
+    }
+
+    public class Kick : ICloseAction
+    {
+        public string execute()
+        {
+            return "kick";
+        }
+    }
+
+    public class Head : ICloseAction
+    {
+        public string execute()
+        {
+            return "head head";
+        }
+    }
+
+    public class Power : IDistinctAction
+    {
+        public override string grab()
+        {
+            return "pick up";
+        }
+
+        public override string hit()
+        {
+            return "slam!";
+        }
+
+        public override List<string> execute()
+        {
+            List<string> list1 = new List<string>();
+            list1.Add(move());
+            list1.Add(grab());
+            list1.Add(hit());
+
+            return list1;
+        }
+    }
+
+    public class Skill : IDistinctAction
+    {
+        public override string grab()
+        {
+            return "hold collar";
+        }
+
+        public override string hit()
+        {
+            return "knee! knee!! knee!!!";
+        }
+
+        public override List<string> execute()
+        {
+
+            List<string> list1 = new List<string>();
+            list1.Add(move());
+            list1.Add(grab());
+            list1.Add(hit());
+            return list1;
+        }
+    }
+
 }
+
+
+
+
+
+
+
