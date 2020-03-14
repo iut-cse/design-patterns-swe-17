4a46ead6709b8f273cbf27dbc3ba6d4da0c98889
30-jubair <jobairt03@gmail.com>
2020-03-14 22:10:45 +0600

    Task3 completed with Applying SetPreference and GetPreference in the test case

--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,7 +1,40 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
         
+        private Preference() { }
+
+        private static Preference instance;
+
+     
+        private IDictionary<string, string> list = new Dictionary<string, string>();
+
+         public static Preference GetInstance()
+        {
+
+            if (instance == null)
+            {
+                instance = new Preference();
+            }
+            return instance;
+        }
+
+
+        public void setPreference(string key, string value)
+        {
+            list[key] = value;
+        }
+
+        public string getPreference(string key)
+        {
+            return list[key]; 
+        }
+
+
+
+
     }
 }
