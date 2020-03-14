71413c3a6fd3971402c183f6ad7ea4d6e2c0aa46
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-03-14 21:14:38 +0600

    problem 3 solved

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
@@ -8,15 +8,20 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void DoTest()
         {
-            Preference instance1 = null; // initialize correctly
+            Preference instance1 = Preference.GetInstance(); // initialize correctly
             // set value "123" with key "abc"
-            string abcValue = null; // get with key "abc";
+           
+            instance1.SetPreference("abc", "123"); // get with key "abc";
+            string abcValue = instance1.GetPreference("abc");
             Assert.Equal("123", abcValue);
 
-            Preference instance2 = null; // initialize correctly
+            Preference instance2 = Preference.GetInstance(); // initialize correctly
             // set value "567" with key "abc" in instance2.
             // get value with key "abc" from instance1.
             // assert
+            instance2.SetPreference("abc", "567");
+            string abcValue2 = instance2.GetPreference("abc");
+            Assert.Equal("567", abcValue2);
         }
     }
 }
--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -2,6 +2,44 @@
 {
     public class Preference
     {
-        // TODO: implement
-    }
-}
+        private static Preference instance;
+        private static readonly object Lock= new object();
+        public string key= null;
+        public string value= null;
+        private Preference(){
+            }
+            public static Preference GetInstance(){
+                if (instance==null)
+                    {
+                    lock(Lock)
+                        {
+                        if(instance==null)
+                            instance=new Preference();
+                        }
+                    }
+                return instance;
+                }
+            public void SetPreference(string key, string value)
+                {
+                if (key==instance.key){
+                   
+                    instance.value= value;
+                }
+                else if(instance.key==null){
+                    instance.key=key;
+                    instance.value=value;
+                }
+            }
+            public string GetPreference(string key)
+                {
+                if (key==instance.key){
+                    
+                    return instance.value;
+                }
+                else 
+                    return null;
+                
+                }
+            }
+ }
+  
