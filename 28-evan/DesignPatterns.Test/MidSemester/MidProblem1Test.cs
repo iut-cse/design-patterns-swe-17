ae888964513cd01adfde12e644d3ba53a6dbb399
Raihan Evan <raihan.evan@gmail.com>
2020-03-14 18:38:17 +0600

    Update MidProblem1Test.cs
    #1 test addes on 'null'

--- 28-evan/DesignPatterns.Test/MidSemester/MidProblem1Test.cs
+++ 28-evan/DesignPatterns.Test/MidSemester/MidProblem1Test.cs
@@ -8,35 +8,35 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void Punch()
         {
-            Ratul ratul = null;
+            Ratul ratul = new ratul(new Punch,new Skill());
             Assert.Equal("punch", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Kick()
         {
-            Ratul ratul = null;
+            Ratul ratul = new ratul(new Kick,new Skill());
             Assert.Equal("kick", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Head()
         {
-            Ratul ratul = null;
+            Ratul ratul = new ratul(new Head,new Skill());
             Assert.Equal("head head", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Power()
         {
-            Ratul ratul = null;
+            Ratul ratul = new ratul(new Kick,new Power());
             Assert.Equal(new[] { "move", "pick up", "slam!" }, ratul.ExecuteDistantAction());
         }
 
         [Fact]
         void Skill()
         {
-            Ratul ratul = null;
+            Ratul ratul = new ratul(new Kick,new Skill());
             Assert.Equal(new[] { "move", "hold collar", "knee! knee!! knee!!!" }, ratul.ExecuteDistantAction());
         }
     }
