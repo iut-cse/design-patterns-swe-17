3fc5a8d8a2af9f91bbe97456f2dd5c48ecd4745e
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-03-14 16:23:07 +0600

    #problemset_3 Preference Class modified

---
+++ 19-zarif/DesignPatterns/DesignPatterns/MidSemester/Problemset_3/Preference.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPatterns.MidSemester
+{
+    public class Preference
+    {
+        private static Preference preference;
+        private static readonly object Lock = new object();
+        private Preference()
+        {
+
+        }
+
+        public static Preference getInstance()
+        {
+            if (preference == null)
+            {
+                lock (Lock)
+                {
+                    if (preference == null)
+                    {
+                        preference = new Preference();
+                    }
+                }
+            }
+            return preference;
+        }
+        void setPreference(string key,string value)
+        {
+
+        }
+
+       
+
+    }
+}
