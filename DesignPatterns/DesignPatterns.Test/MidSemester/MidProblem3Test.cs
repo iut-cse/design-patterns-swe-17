404bd25a2e7e0a67e6b7c26609a6d7e7df84ecd0
raihantitas <aburaihan@iut-dhaka.edu>
2020-03-14 22:03:16 +0600

    task2 is done

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
@@ -8,15 +8,20 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void DoTest()
         {
-            Preference instance1 = null; // initialize correctly
-            // set value "123" with key "abc"
-            string abcValue = null; // get with key "abc";
+            Preference instance1 = Preference.getInstance();
+            instance1.setPreference("abc", "123");
+            
+            string abcValue = instance1.getPreference("abc"); 
             Assert.Equal("123", abcValue);
 
-            Preference instance2 = null; // initialize correctly
-            // set value "567" with key "abc" in instance2.
-            // get value with key "abc" from instance1.
-            // assert
+
+
+            Preference instance2 = Preference.getInstance();
+            instance2.setPreference("abc", "567");
+
+            string abcValue2 = instance2.getPreference("abc");
+            Assert.Equal("567", abcValue2);
+            
         }
     }
 }
--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,7 +1,43 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
-        // TODO: implement
+        IDictionary<string, string> keyValue = new Dictionary<string, string>();
+        private static Preference instance;
+        private static readonly object locking = new object();
+
+        private Preference()
+        {
+
+        }
+        public void setPreference(string key, string value)
+        {
+            if (keyValue.ContainsKey(key))
+            {
+                keyValue.Remove(key);
+            }
+            keyValue.Add(key, value);
+        }
+        public string getPreference(string key)
+        {
+            return keyValue[key];
+        }
+        
+        public static Preference getInstance()
+        {
+            if (instance == null)
+            {
+                lock (locking)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Preference();
+                    }
+                }
+            }
+            return instance;
+        }
     }
 }
