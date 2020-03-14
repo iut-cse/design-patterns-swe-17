a2401edb013aa2fd6ba98ec6570d0bcf890f2177
Raihan Evan <raihan.evan@gmail.com>
2020-03-14 18:33:46 +0600

    New foldercreation with repo files

---
+++ 28-evan/DesignPatterns.Test/MidSemester/MidProblem2Test.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace DesignPatterns.Test.MidSemester
+{
+    public class MidProblem2Test
+    {
+        [Fact]
+        void NoChange()
+        {
+            var original = "Top Score is 305";
+            var converted = ""; // Do the "No Change" conversion.
+            Assert.Equal("Top Score is 305", converted);
+        }
+
+        [Fact]
+        void CompressThenEncrypt()
+        {
+            var original = "Top Score is 305";
+            var converted = ""; // compress then encryppt.
+            Assert.Equal("top score is 3", converted);
+        }
+
+        [Fact]
+        void EncryptThenCompressThenEncodeThenCompress()
+        {
+            var original = "Top Score is 305";
+            var converted = ""; // do the convertion
+            Assert.Equal("(top score is ", converted);
+        }
+
+        [Fact]
+        void CompressThenEncodeThenEncrypt()
+        {
+            // TODO: Implement this;
+            Assert.True(false);
+        }
+    }
+}
---
+++ 28-evan/DesignPatterns/Reports/Kpis/IKpi.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Reports.Kpis
+{
+    public interface IKpi
+    {
+        double Calculate(List<ClassInfo> data, Predicate<ClassInfo> filter);
+    }
+}
