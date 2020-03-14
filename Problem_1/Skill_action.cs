e18d58d2a2ad95463138f5aab898990ebd7424a3
musfiksifat <57532807+musfiksifat@users.noreply.github.com>
2020-03-15 01:03:16 +0600

    Add files via upload
    Task 1 solution added with test

---
+++ Problem_1/Skill_action.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester
+{
+    public class Skill_action : Distant_action
+    {
+        public override string grab()
+        {
+            return "hold collar";
+        }
+
+        public override string hit()
+        {
+            return "knee! knee!! knee!!!";
+        }
+    }
+}
