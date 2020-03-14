9fd892b7194a9ceedb49481580419ee321a5f089
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-03-14 22:17:23 +0600

    mid_solve3

---
+++ 44-rafsan/DesignPatterns/DesignPatterns/MidSemester/IConversion.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester
+{
+    public interface IConversion
+    {
+        public string conversion(string text);
+
+    }
+
+    public class plaintext : IConversion
+    {
+        public string conversion(string text)
+        {
+            return text;
+        }
+    }
+    public class Decorator : IConversion
+    {
+        IConversion stat;
+        public Decorator(IConversion stat)
+        {
+            this.stat = stat;
+        }
+        public virtual string conversion(string text)
+        {
+            return stat.conversion(text);
+        }
+    }
+    public class Encryption : Decorator
+    {
+        public Encryption(IConversion stat) : base(stat)
+        {
+        }
+        public override string conversion(string text)
+        {
+            return base.conversion(text).ToLower();
+        }
+    }
+
+    public class Compression : Decorator
+    {
+        public Compression(IConversion stat) : base(stat)
+        {
+        }
+        public override string conversion(string text)
+        {
+            if (text.Length < 3)
+            {
+                return base.conversion(text);
+            }
+            else
+            {
+                return base.conversion(text).Substring(0, text.Length - 1);
+            }
+
+        }
+    }
+    public class Base_64_encodeing : Decorator
+    {
+        public Base_64_encodeing(IConversion stat) : base(stat)
+        {
+        }
+        public override string conversion(string text)
+        {
+            return "(" + base.conversion(text) + ")";
+        }
+    }
+}
---
+++ 44-rafsan/DesignPatterns/DesignPatterns/MidSemesterTests/Mid_problem2TestCase.cs
@@ -0,0 +1,54 @@
+﻿using DesignPatterns.MidSemester;
+using Xunit;
+
+namespace DesignPatterns.Test.MidSemester
+{
+    public class Mid_problem2TestCase
+    {
+        [Fact]
+        void NoChange()
+        {
+            var original = "Top Score is 305";
+            IConversion stat = new plaintext();
+            var converted = stat.conversion(original);
+            Assert.Equal("Top Score is 305", converted);
+        }
+
+        [Fact]
+        void CompressThenEncrypt()
+        {
+            var original = "Top Score is 305";
+            IConversion stat = new plaintext();
+            stat = new Compression(stat);
+            stat = new Encryption(stat);
+            var converted = stat.conversion(original); // compress then encryppt.
+            Assert.Equal("top score is 3", converted);
+        }
+
+        [Fact]
+        void EncryptThenCompressThenEncodeThenCompress()
+        {
+            var original = "Top Score is 305";
+            IConversion stat = new plaintext();
+            stat = new Encryption(stat);
+            stat = new Compression(stat);
+            stat = new Base_64_encodeing(stat);
+            stat = new Compression(stat);
+            var converted = stat.conversion(original);
+            Assert.Equal("(top score is )", converted);
+        }
+
+        [Fact]
+        void CompressThenEncodeThenEncrypt()
+        {
+            var original = "Top Score is 305";
+            IConversion stat = new plaintext();
+            stat = new Compression(stat);
+            stat = new Base_64_encodeing(stat);
+            stat = new Encryption(stat);
+            var converted = stat.conversion(original);
+            Assert.Equal("(top score is 3)", converted);
+          
+        }
+    }
+}
