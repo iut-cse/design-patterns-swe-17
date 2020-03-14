9a7e0213bc760ef311b7abd493f8435d82297c6e
TasnimAfra-73 <tasnimjarin@iut-dhaka.edu>
2020-03-15 00:47:55 +0600

    #Task 3 completed

--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,7 +1,47 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
-        // TODO: implement
+        private static Preference instance;
+        private static readonly object ThreadLock = new object();
+        private Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+        private Preference() { }
+
+        public static Preference GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (ThreadLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Preference();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public void setPreference(string key, string value)
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary.Add(key, value);
+            }
+
+            else
+            {
+                dictionary[key] = value;
+            }
+        }
+
+        public string getPreference(string key)
+        {
+            return dictionary[key];
+        }
+
     }
 }
