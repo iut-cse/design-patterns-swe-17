3a20f72675c11989a61f59f7738ab8df11c2c147
musfiksifat <57532807+musfiksifat@users.noreply.github.com>
2020-03-15 03:54:30 +0600

    task 2 done

---
+++ IStatistics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester.Problem_2
+{
+    public interface IStatistics
+    {
+        public string statistics(string data);
+    }
+}
