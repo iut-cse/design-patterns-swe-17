8914b5271eab8351cfb73ecd2c7140de1e6bf59c
Lisa Chowdhury <fahmidatasnim@iut-dhaka.edu>
2020-03-14 21:33:03 +0600

    Prefrence class task3

--- DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/MidSemester/Preference.cs
@@ -2,14 +2,45 @@
 {
     public class Preference
     {
-        private static Preference preference;
-        private static object locker = new object();
+        private static object ThreadLock = new object();
+        private static Preference instance;
+        private string TheKey;
+        private string TheValue;
 
         private Preference()
         {
 
         }
-        return preference;
+        public Preference getInstance(string key,string value)
+        {
+            if (instance == null)
+            {
+                lock (ThreadLock)
+                {
+                    {
+                        if (instance == null)
+                            instance = new Preference();
+                    }
+                }
+            }
+
+            return instance;
+        }
+
+        public void setPreference(string key,string value)
+        {
+            TheKey = key;
+            TheValue = value;
+        }
+
+        public string getPrefrence(string key)
+        {
+            if (key.Equals(TheKey))
+                return TheValue;
+
+            else
+                return null;
+        }
 
 
 
