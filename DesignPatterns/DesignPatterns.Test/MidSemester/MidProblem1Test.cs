5c25fe6f7813a254fe2c34a1bcbc956529f85b51
ShahriarHasanAnik <60704388+ShahriarHasanAnik@users.noreply.github.com>
2020-03-15 17:39:38 +0600

    test2
    test2

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem1Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem1Test.cs
@@ -8,35 +8,41 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void Punch()
         {
-            Ratul ratul = new Ratul(new Head(), null);
+            Ratul ratul = new Ratul(new Punch(), new Skill());
+
             Assert.Equal("punch", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Kick()
         {
-            Ratul ratul = new Ratul(new Head(), null);
+            Ratul ratul = new Ratul(new Kick(), new Skill());
+
             Assert.Equal("kick", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Head()
         {
-            Ratul ratul = new Ratul(new Head(), null);
+            Ratul ratul = new Ratul(new Head(), new Skill());
+
             Assert.Equal("head head", ratul.ExecuteCloseAction());
+
         }
 
         [Fact]
         void Power()
         {
-            Ratul ratul = new Ratul(null, new Power());
+            Ratul ratul = new Ratul(new Kick(), new Power());
+
             Assert.Equal(new[] { "move", "pick up", "slam!" }, ratul.ExecuteDistantAction());
         }
 
         [Fact]
         void Skill()
         {
-            Ratul ratul = new Ratul(null, new Skill());
+            Ratul ratul = new Ratul(new Kick(), new Skill());
+
             Assert.Equal(new[] { "move", "hold collar", "knee! knee!! knee!!!" }, ratul.ExecuteDistantAction());
         }
     }
