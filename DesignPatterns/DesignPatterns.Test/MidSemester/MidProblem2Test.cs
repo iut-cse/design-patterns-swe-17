ead3299efaccbf412616cb943fd329ce01254b98
ShahriarHasanAnik <60704388+ShahriarHasanAnik@users.noreply.github.com>
2020-03-15 18:26:56 +0600

    test3
    testclass created and edited

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem2Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem2Test.cs
@@ -7,32 +7,47 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void NoChange()
         {
+
             var original = "Top Score is 305";
+
             var converted = ""; // Do the "No Change" conversion.
+
             Assert.Equal("Top Score is 305", converted);
+
         }
 
         [Fact]
         void CompressThenEncrypt()
         {
+
             var original = "Top Score is 305";
+
             var converted = ""; // compress then encryppt.
+
             Assert.Equal("top score is 3", converted);
+
         }
 
         [Fact]
         void EncryptThenCompressThenEncodeThenCompress()
         {
+
             var original = "Top Score is 305";
+
             var converted = ""; // do the convertion
+
             Assert.Equal("(top score is ", converted);
+
         }
 
         [Fact]
         void CompressThenEncodeThenEncrypt()
         {
+
             // TODO: Implement this;
+
             Assert.True(false);
+
         }
     }
 }
