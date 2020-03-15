4b3d2ecf40f6025bc3a17d86ed0823cddc1ca4ab
ShahriarHasanAnik <60704388+ShahriarHasanAnik@users.noreply.github.com>
2020-03-15 16:50:39 +0600

    Task3
    #3

--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -2,6 +2,57 @@
 {
     public class Preference
     {
-        // TODO: implement
+
+        private static Preference preference;
+
+        private static readonly object oneLock = new object();
+
+        private string value, key;
+        private Preference()
+
+        { 
+
+        }
+        public static Preference getInstance()
+        {
+            if (preference == null)
+            {
+                lock(oneLock)
+                {
+                    if(preference==null)
+                    {
+                        preference = new Preference();
+                    }
+                }
+            }
+            return preference;
+
+
+        }
+        public void setPreference(string key, string value)
+
+        {
+
+            this.value = value;
+
+            this.key = key;
+
+
+
+        }
+
+        public string getPreference(string key)
+        {
+            if(key.Equals(key))
+            {
+                return value;
+            }
+            else
+            {
+                return "null";
+            }
+        }
+
+
     }
 }
