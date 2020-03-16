1ea73fb0833fc3737958b79031c0b6040dc90fb7
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-03-16 06:26:12 +0600

    undo changes done by nafis ahmed

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem1Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem1Test.cs
@@ -8,35 +8,35 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void Punch()
         {
-            Ratul ratul = new Ratul(new Head(), null);
+            Ratul ratul = null;
             Assert.Equal("punch", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Kick()
         {
-            Ratul ratul = new Ratul(new Head(), null);
+            Ratul ratul = null;
             Assert.Equal("kick", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Head()
         {
-            Ratul ratul = new Ratul(new Head(), null);
+            Ratul ratul = null;
             Assert.Equal("head head", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Power()
         {
-            Ratul ratul = new Ratul(null, new Power());
+            Ratul ratul = null;
             Assert.Equal(new[] { "move", "pick up", "slam!" }, ratul.ExecuteDistantAction());
         }
 
         [Fact]
         void Skill()
         {
-            Ratul ratul = new Ratul(null, new Skill());
+            Ratul ratul = null;
             Assert.Equal(new[] { "move", "hold collar", "knee! knee!! knee!!!" }, ratul.ExecuteDistantAction());
         }
     }
