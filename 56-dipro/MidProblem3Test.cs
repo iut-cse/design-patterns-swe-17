0411195c5062f2559d65ab8f01635461d35ef695
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-03-17 14:44:36 +0600

    Task-3

--- 56-dipro/MidProblem3Test.cs
+++ 56-dipro/MidProblem3Test.cs
@@ -8,16 +8,16 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void DoTest()
         {
-          
+            
             Preference instance1 = Preference.GetInstance();
-            instance1.SetPreference("abc","123");
-            string abcValue = instance1.GetPreference("abc"); 
+            instance1.SetPreference("abc", "123");         
+            string abcValue = instance1.GetPreference("abc");
             Assert.Equal("123", abcValue);
 
-            Preference instance2 = Preference.GetInstance(); 
-            instance2.SetPreference("abc","567");
+            Preference instance2 = Preference.GetInstance();
+            instance2.SetPreference("abc", "567");
             string _abcvalue = instance1.GetPreference("abc");
-            Assert.Equal("567",_abcvalue);
+            Assert.Equal("567", _abcvalue);
         }
     }
 }
--- 56-dipro/Preference.cs
+++ 56-dipro/Preference.cs
@@ -1,22 +1,23 @@
 ﻿using System.Collections.Generic;
+
 namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
         private static Preference instance;
-        private static object locker = new object();
-        private Dictionary<string, string> storage;
+        private static object locker=new object();
+        private Dictionary <string,string> storage;
 
         public static Preference GetInstance()
         {
-            if (instance == null)
+            if(instance==null)
             {
-                lock (locker)
-                {
-                    if (instance == null)
-                        instance = new Preference();
+                lock(locker)
+            {
+                if(instance==null)
+                   instance=new Preference();
 
-                }
+            }
 
             }
 
@@ -25,12 +26,12 @@ namespace DesignPatterns.MidSemester
 
         private Preference()
         {
-            storage = new Dictionary<string, string>();
+            storage=new Dictionary <string,string>();
         }
 
-        public void SetPreference(string key, string value)
+        public void SetPreference(string key,string value)
         {
-            storage[key] = value;
+            storage[key]=value;
         }
 
         public string GetPreference(string key)
