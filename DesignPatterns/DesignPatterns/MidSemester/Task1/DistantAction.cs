2ed169a76db9c2101557fcdf323e88077502c437
Mahamudur Rahaman Khan <44021897+khansun@users.noreply.github.com>
2020-03-17 15:38:10 +0600

    abstract class distant action

---
+++ DesignPatterns/DesignPatterns/MidSemester/Task1/DistantAction.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatterns.MidSemester.Task1
+{
+
+	public abstract class DistantAction
+	{
+		public string Move()
+		{
+			return "Move";
+		}
+		public abstract string Grab();
+		public abstract string Hit();
+		public IEnumerable<string> ExecuteDistantAction()
+		{	
+			yield return Move();
+			yield return Grab();
+			yield return Hit();
+
+		}
+	}
+}
