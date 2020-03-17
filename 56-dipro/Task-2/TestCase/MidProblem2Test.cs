288cf833fd3885c3859954e9b202453503e6c3c3
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-03-17 16:21:51 +0600

    Task-2

--- 56-dipro/Task-2/TestCase/MidProblem2Test.cs
+++ 56-dipro/Task-2/TestCase/MidProblem2Test.cs
@@ -9,7 +9,7 @@ namespace DesignPatterns.Test.MidSemester
         {
             var original = "Top Score is 305";
             GamerStat gamer = new GamerStat();
-            var converted = gamer.Stat(original);
+            var converted = gamer.Stat(original); // Do the "No Change" conversion.
             Assert.Equal("Top Score is 305", converted);
         }
 
@@ -18,7 +18,7 @@ namespace DesignPatterns.Test.MidSemester
         {
             var original = "Top Score is 305";
             BaseDecorator gamer = new EncryptorDecorator(new CompressorDecorator(new GamerStat()));
-            var converted = gamer.Stat(original); 
+            var converted = gamer.Stat(original); // compress then encryppt.
             Assert.Equal("top score is 3", converted);
         }
 
@@ -27,7 +27,7 @@ namespace DesignPatterns.Test.MidSemester
         {
             var original = "Top Score is 305";
             BaseDecorator gamer = new CompressorDecorator(new EncoderDecorator(new CompressorDecorator(new EncryptorDecorator(new GamerStat()))));
-            var converted = gamer.Stat(original); 
+            var converted = gamer.Stat(original); // do the convertion
             Assert.Equal("(top score is ", converted);
         }
 
---
+++ DesignPatterns/DesignPatterns/MidSemester/BaseDecorator.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.MidSemester
+{
+    public abstract class BaseDecorator : IGamerStat
+    {
+        public IGamerStat DecoratorGamerStat;
+
+        public BaseDecorator(IGamerStat _decoratorGamerStat)
+        {
+            this.DecoratorGamerStat = _decoratorGamerStat;
+        }
+        public virtual string Stat(string stat)
+        {
+            return DecoratorGamerStat.Stat(stat);
+        }
+    }
+}
