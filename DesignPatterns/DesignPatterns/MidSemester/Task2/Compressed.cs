8c10964a8fcf2c5c3d767cf13e7f0cf2e7f4cbb8
Mahamudur Rahaman Khan <44021897+khansun@users.noreply.github.com>
2020-03-18 19:00:24 +0600

    compressed class implemented

---
+++ DesignPatterns/DesignPatterns/MidSemester/Task2/Compressed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.MidSemester.Task2
+{
+    class Compressed : DecoratedTxt
+    {
+        public IStatistics stat;
+
+        public Compressed(IStatistics stat) : base(stat)
+        {
+        }
+
+        public override string convertTxt(string statictics)
+        {
+            if (statictics.Length < 3)
+            {
+                return base.convertTxt(statictics);
+            }
+            else
+            {
+                return base.convertTxt(statictics).Substring(0, statictics.Length - 2);
+            }
+        }
+    }
+}
