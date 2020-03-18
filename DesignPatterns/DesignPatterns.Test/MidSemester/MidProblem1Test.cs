af56d71cee9dc1a8b41ebc029cdb13ec1de12db6
alfayedarnob007 <50393946+alfayedarnob007@users.noreply.github.com>
2020-03-18 15:45:07 +0600

    Add files via upload

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem1Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem1Test.cs
@@ -8,35 +8,54 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void Punch()
         {
+<<<<<<< Updated upstream
             Ratul ratul = null;
+=======
+            Ratul ratul = new Ratul(new PunceAction(), new PowerAction());
+>>>>>>> Stashed changes
             Assert.Equal("punch", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Kick()
         {
+<<<<<<< Updated upstream
             Ratul ratul = null;
+=======
+            Ratul ratul = new Ratul(new KickAction(), new PowerAction());
+>>>>>>> Stashed changes
             Assert.Equal("kick", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Head()
         {
+<<<<<<< Updated upstream
             Ratul ratul = null;
+=======
+            Ratul ratul = new Ratul(new HeadAction(), new PowerAction());
+>>>>>>> Stashed changes
             Assert.Equal("head head", ratul.ExecuteCloseAction());
         }
 
         [Fact]
         void Power()
         {
-            Ratul ratul = null;
+           Ratul ratul = null;
+
+            Ratul ratul = new Ratul(new PunceAction(), new PowerAction());
+
             Assert.Equal(new[] { "move", "pick up", "slam!" }, ratul.ExecuteDistantAction());
         }
 
         [Fact]
         void Skill()
         {
+<<<<<<< Updated upstream
             Ratul ratul = null;
+=======
+            Ratul ratul = new Ratul(new PunceAction(), new SkillAction());
+>>>>>>> Stashed changes
             Assert.Equal(new[] { "move", "hold collar", "knee! knee!! knee!!!" }, ratul.ExecuteDistantAction());
         }
     }
