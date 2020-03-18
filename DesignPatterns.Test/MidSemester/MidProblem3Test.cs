37a15ffc452eb7ac42fd165d63484b5d5622c30d
musfiksifat <musfiksifat766@gmail.com>
2020-03-18 17:35:50 +0600

    Revert "Resubmission of all tasks"
    This reverts commit be53b30c92831abb27e9bc8db9899274fb3d3f9a.

---
+++ DesignPatterns.Test/MidSemester/MidProblem3Test.cs
@@ -0,0 +1,23 @@
+﻿using DesignPatterns.MidSemester;
+using Xunit;
+
+namespace DesignPatterns.Test.MidSemester
+{
+    public class MidProblem3Test
+    {
+        [Fact]
+        void DoTest()
+        {
+            Preference instance1 = Preference.getInstance();
+            instance1.setPreference("abc", "123"); // set value "123" with key "abc"
+            string abcValue = instance1.getPreference("abc"); // get with key "abc";
+            Assert.Equal("123", abcValue);
+
+
+            Preference instance2 = Preference.getInstance();
+            instance2.setPreference("abc", "567");  // set value "567" with key "abc" in instance2.
+            string abcValue2 = instance1.getPreference("abc");// get value with key "abc" from instance1.
+            Assert.Equal("123", abcValue2);
+        }
+    }
+}
---
+++ Problem_1/Ratul.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
+{
+    public class Ratul
+    {
+        private I_CloseAction closeAction;
+        private Distant_action distant_Action;
+        public Ratul(I_CloseAction closeAction, Distant_action distant_Action)
+        {
+            this.closeAction = closeAction;
+            this.distant_Action = distant_Action;
+        }
+
+        public string ExecuteCloseAction()
+        {
+            return closeAction.execute_hit();
+        }
+
+        public IEnumerable<string> ExecuteDistantAction()
+        {
+            // you can return a array or list of string. Both array and list are subtypes of IEnumerable
+            // However, there is a thing called `yield return` in C#.
+            // Using it would be "cool"
+            // Make sure you understand it if you choose to use it.
+           
+            return distant_Action.distant_template();
+        }
+    }
+}
