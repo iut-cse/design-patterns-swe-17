8cee844be4adbee9374bec1e6bb35c272ef90cb6
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-03-20 03:25:07 +0600

    Head class is made

---
+++ DesignPatterns/DesignPatterns/MidSemester/Head.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
+{
+
+	public class Head : CloseAction 
+	{
+		public string execute()
+        {
+			return "Head Shot";
+        }
+	}
+}
