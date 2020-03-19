3c3adff6ea9bd50d0bbfbea00a0769ac12b20915
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-03-19 22:54:03 +0600

    Problem Solve

--- DesignPatterns/DesignPatterns/MidSemester/Skill.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Skill.cs
@@ -3,16 +3,12 @@ using System.Text;
 using System.Collections.Generic;
 namespace DesignPatterns.MidSemester
 {
-    public class Skill : DistantAction
-    {
-        public override string Grab()
-        {
-            return "hold collar";
-        }
-
-        public override string Hit()
-        {
+    public class Skill : DistantAction{
+        public override string Hit(){
             return "knee! knee!! knee!!!";
         }
+        public override string Grab(){
+            return "hold collar";
+        } 
     }
 }
