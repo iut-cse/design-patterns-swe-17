69d4d4fb4f9ae13b88624b85b03b9cec2b9bd0ae
MD SHAJAHAN HOSSAIN <MD SHAJAHAN HOSSAIN@DESKTOP-NQ34G15>
2020-03-19 20:05:22 +0600

    #no-1 & no-3 done

--- DesignPatterns/DesignPatterns/MidSemester/Preference.cs
+++ DesignPatterns/DesignPatterns/MidSemester/Preference.cs
@@ -1,7 +1,37 @@
-﻿namespace DesignPatterns.MidSemester
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
 {
     public class Preference
     {
         // TODO: implement
+        private static Preference instance;
+
+        private Preference()
+        {
+
+        }
+
+        private IDictionary<string, string> dic = new Dictionary<string, string>();
+
+        public static Preference getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Preference();
+            }
+            return instance;
+        }
+
+        public void setPreferance(string key, string value)
+        {
+            dic[key] = value;
+        }
+
+        public string getPreferance(string key)
+        {
+            return dic[key];
+        }
+
     }
 }
