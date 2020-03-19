ce56c778ee1ca95cdb432a0ebdf04c43f3f43b5a
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-03-20 01:44:13 +0600

    Class SkillAction is created

---
+++ DesignPatterns/DesignPatterns/MidSemester/SkillAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
+{
+    public class SkillAction : DistantAction
+    {
+        public override string Grab()
+        {
+            return "Hold Collar";
+        }
+        public override string Hit()
+        {
+            return "Hit with Knee";
+        }
+    }
+}
+
+
