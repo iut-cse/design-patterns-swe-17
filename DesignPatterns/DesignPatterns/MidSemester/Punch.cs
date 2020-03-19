c846d063a95c1f3e80712332f0599f2911e8854b
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-03-20 02:14:58 +0600

    Punch class is added

---
+++ DesignPatterns/DesignPatterns/MidSemester/Punch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.MidSemester
+{
+    public class Punch : CloseAction
+    {
+        public string execute()
+        {
+            return "Punching Wohooaa";
+        }
+    }
+}
+
+
