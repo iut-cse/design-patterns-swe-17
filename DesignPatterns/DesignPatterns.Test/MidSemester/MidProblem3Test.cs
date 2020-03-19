798bb63044512f257d4eb9d2a2e4204fcd426fca
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-03-20 01:26:36 +0600

    Test fot Task 3 is Completed

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
@@ -1,22 +1,23 @@
-﻿using DesignPatterns.MidSemester;
+﻿using DesignPatterns.MidSemester.Problem3;
 using Xunit;
 
-namespace DesignPatterns.Test.MidSemester
+namespace DesignPatterns.Test.MidSemester.Problem3
 {
     public class MidProblem3Test
     {
         [Fact]
         void DoTest()
         {
-            Preference instance1 = null; // initialize correctly
+            Preference instance1 = Preference.getInstance(); // initialize correctly
             // set value "123" with key "abc"
-            string abcValue = null; // get with key "abc";
+            instance1.setPreference("abc","123");
+            string abcValue = instance1.getPreference("abc"); // get with key "abc";
             Assert.Equal("123", abcValue);
 
-            Preference instance2 = null; // initialize correctly
-            // set value "567" with key "abc" in instance2.
-            // get value with key "abc" from instance1.
-            // assert
+            Preference instance2 = Preference.getInstance(); // initialize correctly
+            instance1.setPreference("abc", "567"); // set value "567" with key "abc" in instance2.
+            string abcvalue = instance1.getPreference("abc");// get value with key "abc" from instance1.
+            Assert.Equal("567", abcvalue);// assert
         }
     }
 }
